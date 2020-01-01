fc3eb870761f318ffa23000d6c30ccb1f61b4185
Carles Capellas <carles.capellas@sage.com>
2020-01-01 20:43:01 +0100

    feat: improve glossary

--- src/components/glossary.tsx
+++ src/components/glossary.tsx
@@ -4,28 +4,32 @@ export const Glossary = () => (
     <React.Fragment>
         <h2>Glosario</h2>
 
-        <h3>Leader</h3>
-        <p>Persona que dirige el baile (habitualmente el chico en una pareja chico-chica)</p>
-        <h3>Follower</h3>
-        <p>Persona que se deja guiar (habitualmente la chica en una pareja chico-chica)</p>
+        <p><b>Leader</b>: Persona que dirige el baile (habitualmente el chico en una pareja chico-chica)</p>
 
-        <h3>Vuelta</h3>
-        <p>Leader y/o follower giran respeto su propio eje central (por ejemplo, vuelta de reloj)</p>
-        <h3>Giro</h3>
-        <p>Leader y/o follower giran respeto un eje fuera de su centro (por ejemplo, giro en el sitio)</p>
+        <p><b>Follower</b>: Persona que se deja guiar (habitualmente la chica en una pareja chico-chica)</p>
 
-        <h3>Mano avance</h3>
-        <p>La mano del lado correspondiente a la dirección de avance lateral (cuando los brazos no estan cruzados). Por ejemplo</p>
+        <p><b>Vuelta</b>: Leader y/o follower giran respeto su propio eje central (por ejemplo, vuelta de reloj)</p>
+
+        <p><b>Giro</b>: Leader y/o follower giran respeto un eje fuera de su centro (por ejemplo, giro en el sitio)</p>
+
+        <p><b>Mano avance</b>: La mano del lado correspondiente a la dirección de avance lateral (cuando los brazos no estan cruzados). Por ejemplo:</p>
         <ul>
             <li>En los tiempos 1, 2, 3 y 4 de un basico lateral, la mano izquierda del leader y la mano derecha del follower</li>
             <li>En los tiempos 5, 6, 7 y 8 de un basico lateral, la mano derecha del leader y la mano izquierda del follower</li>
         </ul>
 
-        <h3>Mano arrastre</h3>
-        <p>La mano del lado opuesto a la dirección de avance lateral (cuando los brazos no estan cruzados). Por ejemplo</p>
+        <p><b>Mano arrastre</b>: La mano del lado opuesto a la dirección de avance lateral (cuando los brazos no estan cruzados). Por ejemplo</p>
         <ul>
             <li>En los tiempos 1, 2, 3 y 4 de un basico lateral, la mano derecha del leader y la mano izquierda del follower</li>
             <li>En los tiempos 5, 6, 7 y 8 de un basico lateral, la mano izquierda del leader y la mano derecha del follower</li>
+        </ul>
+
+        <h3>Tiempos</h3>
+        <ul>
+            <li>Tiempo pico: el 1 o el 5</li>
+            <li>Tiempo pluma: el 2 o el 6</li>
+            <li>Tiempo ala: el 3 o el 7</li>
+            <li>Tiempo cola: el 4 o el 8</li>
         </ul>
     </React.Fragment>
 );
