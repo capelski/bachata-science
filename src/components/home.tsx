44aa027eeff7287a9281ba9cef3ac2a2f42c06a1
Carles Capellas <carles.capellas@sage.com>
2020-01-10 08:25:49 +0100

    feat: add info to home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -7,19 +7,18 @@ export const Home = () => (
         <h1>Bachata Science</h1>
 
         <p>
-            Esta página pretende ser un catalogo de los pasos mas comunes de bachata, para aprender
-            como se hacen, enseñarlos y sobretodo, establecer un nombre único para cada uno.
+            Esta página pretende ser un catalogo de los pasos mas comunes de bachata, para recordar
+            como se hacen, enseñarlos y sobretodo, establecer un nombre único para cada uno. Puede
+            que los nombres no te gusten (acepto sugerencias mejores) o que eches de menos tu paso
+            preferido (en cuyo caso, envíame un video del paso a <b>capellas.carles@gmail.com</b> y
+            lo añadire).Verás que utilizo términos inventados para definir los pasos; tal vez te
+            ayude echar un vistazo al <Link to="/glossary?$modena=bachata-science">glosario</Link>
         </p>
 
         <p>
-            Puede que los nombres no te gusten (acepto sugerencias mejores) o que eches de menos tu
-            paso preferido (en cuyo caso, escríbeme a <b>capellas.carles@gmail.com</b> explicando
-            como se hace y lo añadire).
-        </p>
-
-        <p>
-            Verás que utilizo términos inventados para definir los pasos; tal vez te ayude echar un
-            vistazo al <Link to="/glossary?$modena=bachata-science">glosario</Link>
+            <b>Atención! Con esta página no aprenderás a bailar</b>. Lo mas que vas a lograr es
+            convertirte en un bachatero robot. La única forma de aprender es practicar primero los
+            pasos en la tranquilidad de una clase y consolidarlos luego en un social.
         </p>
     </React.Fragment>
 );
