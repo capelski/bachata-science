31e12033a1f8608f6ddcff4c7c2b8f67a9b87f84
Carles Capellas <capellas.carles@gmail.com>
2021-04-18 11:18:20 +0200

    feat: prerender positions and steps routes

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,11 +1,17 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const { PositionId } = require('../pre-rendering/positions');
+const { StepId } = require('../pre-rendering/steps');
 const baseConfig = require('./base.config');
 
-const routes = ['/', '/glossary', '/positions', '/stages', '/steps'].map(
-    route => `/bachata-science${route}`
-);
+const positionRoutes = Object.values(PositionId).map(positionId => `/position/${positionId}`);
+const stepRoutes = Object.values(StepId).map(stepId => `/step/${stepId}`);
+
+const routes = ['/', '/glossary', '/positions', '/stages', '/steps']
+    .concat(positionRoutes)
+    .concat(stepRoutes)
+    .map(route => `/bachata-science${route}`);
 
 module.exports = merge(baseConfig, {
     mode: 'production',
